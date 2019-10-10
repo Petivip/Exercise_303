312c813cb41c74ff211b907ec108500fdd4ce0ae
petivip <petivip@gmail.com>
2019-10-09 22:04:32 -0400

    springboot303

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+
+import com.sun.istack.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 4)
+    private String name;
+
+    @NotNull
+    @DateTimeFormat
+    private int duedate;
+
+    @NotNull
+    private int priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(int duedate) {
+        this.duedate = duedate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
